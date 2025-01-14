958ba80a9cdb8c6983e28cc539d575de0ed35784
Jooyoung Yoo <43409521+Jooyoung01@users.noreply.github.com>
2025-01-14 14:19:39 -0800

    Bug Fix

--- utils/img_matching.py
+++ utils/img_matching.py
@@ -1,15 +1,18 @@
 import glob
 import os
-def img_matching_check(gt_data, img_paths=[]) :
+
+def img_matching_check(gt_data, img_paths=[]):
+    print(img_paths)
     error_check = 0
     img_tag_matched_by_path = {path: [] for path in img_paths}
     # img_tag_mached = []
     for gt_data_each in gt_data:
         for img_name in gt_data_each.keys():
-            # print(img_name)
-            file_found = False  #file found flag
+            print(img_name)
+            file_found = False  # file found flag
+            img_name_with_ext = img_name + ".jpg" 
             for img_path in img_paths:
-                full_path = os.path.join(img_path, img_name)
+                full_path = os.path.join(img_path, img_name_with_ext) 
                 matching_files = glob.glob(full_path)
                 if matching_files:
                     file_found = True
@@ -17,7 +20,7 @@ def img_matching_check(gt_data, img_paths=[]) :
                     break
         if not file_found:
             error_check += 1
-            print(f"Error: {img_name} not found.")
+            print(f"Error: {img_name} not found.") 
     for path, matched_files in img_tag_matched_by_path.items():
         print(f"Total # of images in {path} : {sum(1 for file in os.listdir(path) if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp')))}")
         print(f"Total # of images matching gt_data : {len(matched_files)}")
